72c67db828a5b09447cae1a1bca56bf925899b21
athreya-harshith <athreya1207@gmail.com>
2023-10-16 19:35:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/home',(req,res)=>{
+    res.json({message:'OK'});
+})
+app.listen(3000,()=>{
+    console.log('server is up in the port ',3000);
+});
